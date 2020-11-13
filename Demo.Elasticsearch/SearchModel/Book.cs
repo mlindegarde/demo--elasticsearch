dbfaae371eaa77c525bfe8f75104c2669d0feb2c
Michael J Lindegarde <mlindegarde@e2g.com>
2020-11-13 09:36:42 -0500

    Added updatebyquery example.

---
+++ Demo.Elasticsearch/SearchModel/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nest;
+
+namespace Demo.Elasticsearch.SearchModel
+{
+    public class Book
+    {
+        #region Properties
+        [Keyword]
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        #endregion
+    }
+}
