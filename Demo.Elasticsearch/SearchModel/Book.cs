2f5a4b93ed51e61ce81a355e7fd3e5f31126000b
Michael J Lindegarde <mlindegarde@e2g.com>
2020-11-13 10:23:28 -0500

    Id other than id.

--- Demo.Elasticsearch/SearchModel/Book.cs
+++ Demo.Elasticsearch/SearchModel/Book.cs
@@ -3,6 +3,9 @@ using Nest;
 
 namespace Demo.Elasticsearch.SearchModel
 {
+    // If you want to use something other than Id is the _id of the document, add
+    // the following attribute
+    //[ElasticsearchType(IdProperty = nameof(Name))]
     public class Book
     {
         #region Properties
